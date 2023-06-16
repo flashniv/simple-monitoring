eb20a1909f7c1fde47e5c390fea72a3e3e6fb0cd
Ivan N <flashniv@gmail.com>
2023-06-16 14:14:59 +0300

    Add application.cron.runCheckTriggers parameter

--- src/main/java/ua/com/serverhelp/simplemonitoring/config/Cron.java
+++ src/main/java/ua/com/serverhelp/simplemonitoring/config/Cron.java
@@ -2,11 +2,16 @@ package ua.com.serverhelp.simplemonitoring.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import ua.com.serverhelp.simplemonitoring.service.DataItemsService;
 import ua.com.serverhelp.simplemonitoring.service.TriggerService;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Configuration
 @EnableScheduling
@@ -17,23 +22,30 @@ public class Cron {
     @Autowired
     private TriggerService triggerService;
 
+    @Value("${application.cron.runCheckTriggers:true}")
+    private boolean runCheckTriggers;
+
     @Scheduled(fixedDelay = 30000, initialDelay = 90000)
     public void processDataItemQueue() {
+        Instant start = Instant.now();
         log.debug("Cron::processDataItemQueue start");
         try {
             dataItemsService.processItems();
         } catch (Exception e) {
             log.error("Error metric writing", e);
         }
-        log.debug("Cron::processDataItemQueue done");
+        log.debug("Cron::processDataItemQueue done " + Duration.between(start, Instant.now()).get(ChronoUnit.MILLIS));
     }
 
     @Scheduled(fixedDelay = 60000, initialDelay = 90000)
     public void checkTriggers() {
-        log.debug("Cron::checkTriggers start");
+        if (runCheckTriggers) {
+            Instant start = Instant.now();
+            log.debug("Cron::checkTriggers start");
 
-        triggerService.cronCheckTriggers();
+            triggerService.cronCheckTriggers();
 
-        log.debug("Cron::checkTriggers done");
+            log.debug("Cron::checkTriggers done " + Duration.between(start, Instant.now()).get(ChronoUnit.MILLIS));
+        }
     }
 }
